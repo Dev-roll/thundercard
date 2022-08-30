2eba73befb9efe82bcfaba4f2031875b7e5e368c
notchcoder <kokiuaxdx@gmail.com>
2022-08-30 19:08:11 +0900

    feat: ページ遷移の実装、システムテーマの適用

---
+++ lib/Account.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Account extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello, account!'),
+      ),
+    );
+  }
+}
