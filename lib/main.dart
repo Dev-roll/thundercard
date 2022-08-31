ccbd2ec00336b2b7c7c60719cddf948e043ed475
keigomichi <hundredacrewood1165g7@gmail.com>
2022-08-31 16:24:36 +0900

    feat: connect firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,105 @@
+// import 'package:flutter/material.dart';
+// import 'package:thundercard/constants.dart';
+// import 'package:thundercard/login.dart';
+// import 'package:thundercard/thundercard.dart';
+// import 'package:thundercard/list.dart';
+// import 'package:thundercard/notifications.dart';
+// import 'package:thundercard/account.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Thundercard_app',
+//       theme: ThemeData(
+//         useMaterial3: true,
+//         colorSchemeSeed: seedColor,
+//         brightness: Brightness.light,
+//       ),
+//       darkTheme: ThemeData(
+//         useMaterial3: true,
+//         colorSchemeSeed: seedColor,
+//         brightness: Brightness.dark,
+//       ),
+//       // home: const MyHomePage(title: 'Thundercard'),
+//       home: const Login(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   final String title;
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int currentPageIndex = 0;
+//   String userName = 'Sam';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: NavigationBar(
+//         onDestinationSelected: (int index) {
+//           setState(() {
+//             currentPageIndex = index;
+//           });
+//         },
+//         selectedIndex: currentPageIndex,
+//         destinations: const <Widget>[
+//           NavigationDestination(
+//             selectedIcon: Icon(Icons.contact_mail),
+//             icon: Icon(Icons.contact_mail_outlined),
+//             label: 'Tundercard',
+//           ),
+//           NavigationDestination(
+//             selectedIcon: Icon(Icons.view_list_outlined),
+//             icon: Icon(Icons.list_alt_rounded),
+//             label: 'List',
+//           ),
+//           NavigationDestination(
+//             selectedIcon: Icon(Icons.notifications_rounded),
+//             icon: Icon(Icons.notifications_none_rounded),
+//             label: 'Notifications',
+//           ),
+//           NavigationDestination(
+//             selectedIcon: Icon(Icons.account_circle_rounded),
+//             icon: Icon(Icons.account_circle_outlined),
+//             label: 'Account',
+//           ),
+//         ],
+//       ),
+//       body: <Widget>[
+//         Thundercard(name: userName),
+//         List(),
+//         Notifications(),
+//         Account(),
+//       ][currentPageIndex],
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:thundercard/constants.dart';
-import 'package:thundercard/login.dart';
-import 'package:thundercard/thundercard.dart';
-import 'package:thundercard/list.dart';
-import 'package:thundercard/notifications.dart';
-import 'package:thundercard/account.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,80 +109,63 @@ void main() async {
   runApp(const MyApp());
 }
 
+// MyApp,MyHomePageはデフォルトから変更がないため省略
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Thundercard_app',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: seedColor,
-        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
       ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: seedColor,
-        brightness: Brightness.dark,
-      ),
-      // home: const MyHomePage(title: 'Thundercard'),
-      home: const Login(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentPageIndex = 0;
-  String userName = 'Sam';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.contact_mail),
-            icon: Icon(Icons.contact_mail_outlined),
-            label: 'Tundercard',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.view_list_outlined),
-            icon: Icon(Icons.list_alt_rounded),
-            label: 'List',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.notifications_rounded),
-            icon: Icon(Icons.notifications_none_rounded),
-            label: 'Notifications',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.account_circle_rounded),
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Account',
-          ),
-        ],
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      body: <Widget>[
-        Thundercard(name: userName),
-        List(),
-        Notifications(),
-        Account(),
-      ][currentPageIndex],
+      body: Center(
+        child: TextButton(
+          // ボタンを押した時のイベント
+          onPressed: () async {
+            FirebaseFirestore.instance
+                .doc('autoCollection1/autoDocument1')
+                .set({'autofield': "abc"});
+            // FirebaseFirestore.instance
+            //     .collection('autoCollection2')
+            //     .add({'autofield': "xyz"});
+            FirebaseFirestore.instance
+                .collection('flutterDataCollection')
+                .doc('flutterDataDocument')
+                .get()
+                .then((ref) {
+              print(ref.get("mydata"));
+            });
+          },
+          child: const Text(
+            '実行',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
     );
   }
 }
