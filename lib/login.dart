5f5c408e22f6df1d892bbeecd8bfa7a5bf2abdbc
cardseditor <toshiki.u314@gmail.com>
2022-09-15 16:43:04 +0900

    fix: login with another account

--- lib/login.dart
+++ lib/login.dart
@@ -68,7 +68,7 @@ class _LoginState extends State<Login> {
                     final User? user = (await FirebaseAuth.instance
                             .signInWithEmailAndPassword(
                                 // email: _email, password: _password))
-                                email: _email == '' ? _email : 'example@example.com', password: _password == '' ? _password : 'hogehoge'))
+                                email: _email == '' ? 'example@example.com' : _email, password: _password == '' ? 'hogehoge' : _password))
                         .user;
                     if (user != null) {
                       print("ログインしました　${user.email} , ${user.uid}");
