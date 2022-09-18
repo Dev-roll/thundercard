0cb0b1121e7e8a2f84a6cbaa7ed7a805f64236ae
keigomichi <hundredacrewood1165g7@gmail.com>
2022-09-18 23:57:29 +0900

    chore: display account info based on uid

--- lib/Account.dart
+++ lib/Account.dart
@@ -15,64 +15,88 @@ class Account extends StatefulWidget {
 
 class _AccountState extends State<Account> {
   final String? uid = getUid();
-  final cardId = 'keigomichi';
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('アカウント'),
       ),
-      body: SafeArea(
-        child: Scrollbar(
-          child: SingleChildScrollView(
-              child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                    stream: FirebaseFirestore.instance
-                        .collection('cards')
-                        .doc(cardId)
-                        .snapshots(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<DocumentSnapshot> snapshot) {
-                      if (snapshot.hasError) {
-                        return const Text('Something went wrong');
-                      }
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Text("Loading");
-                      }
-                      dynamic data = snapshot.data;
-                      return Column(
-                        children: [
-                          OutlinedButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) =>
-                                      AccountEditor(data: data, cardId: cardId),
-                                ));
-                              },
-                              child: const Text('プロフィールを編集')),
-                          Text('username: ${data?['name']}'),
-                          Text('bio: ${data?['bio']}'),
-                          Text('URL: ${data?['url']}'),
-                          Text('Twitter: ${data?['twitter']}'),
-                          Text('GitHub: ${data?['github']}'),
-                          Text('company: ${data?['company']}'),
-                          Text('email: ${data?['email']}'),
-                          Image.network(data?['thumbnail']),
+      body: FutureBuilder<DocumentSnapshot>(
+          future: users.doc(uid).get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return Text("Document does not exist");
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> user =
+                  snapshot.data!.data() as Map<String, dynamic>;
+
+              return SafeArea(
+                child: Scrollbar(
+                  child: SingleChildScrollView(
+                      child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                            stream: FirebaseFirestore.instance
+                                .collection('cards')
+                                .doc(user['my_cards'][0])
+                                .snapshots(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<DocumentSnapshot> snapshot) {
+                              if (snapshot.hasError) {
+                                return const Text('Something went wrong');
+                              }
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const Text("Loading");
+                              }
+                              dynamic data = snapshot.data;
+                              return Column(
+                                children: [
+                                  OutlinedButton(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .push(MaterialPageRoute(
+                                          builder: (context) => AccountEditor(
+                                              data: data,
+                                              cardId: user['my_cards'][0]),
+                                        ));
+                                      },
+                                      child: const Text('プロフィールを編集')),
+                                  Text('username: ${data?['name']}'),
+                                  Text('bio: ${data?['bio']}'),
+                                  Text('URL: ${data?['url']}'),
+                                  Text('Twitter: ${data?['twitter']}'),
+                                  Text('GitHub: ${data?['github']}'),
+                                  Text('company: ${data?['company']}'),
+                                  Text('email: ${data?['email']}'),
+                                  Image.network(data?['thumbnail']),
+                                ],
+                              );
+                            },
+                          ),
                         ],
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          )),
-        ),
-      ),
+                      ),
+                    ),
+                  )),
+                ),
+              );
+            }
+
+            return Text("loading");
+          }),
     );
   }
 }
