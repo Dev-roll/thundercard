96d2252417cdfb359470bf866e6dfb7284e755ae
keigomichi <hundredacrewood1165g7@gmail.com>
2022-09-19 15:55:41 +0900

    chore: wrap futurebuilder and display MyCard widget if is_user is true

--- lib/List.dart
+++ lib/List.dart
@@ -1,19 +1,12 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:thundercard/upload_image_page.dart';
 import 'package:thundercard/widgets/chat/room_list_page.dart';
-import 'dart:io';
-import 'package:flutter/services.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:thundercard/widgets/my_card.dart';
+import 'api/firebase_auth.dart';
 
 class List extends StatefulWidget {
   const List({Key? key, required this.uid}) : super(key: key);
-  // const List({Key? key, required this.uid}) : super(key: key);
   final String? uid;
 
   @override
@@ -21,9 +14,10 @@ class List extends StatefulWidget {
 }
 
 class _ListState extends State<List> {
-  File? image;
+  final String? uid = getUid();
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  CollectionReference cards = FirebaseFirestore.instance.collection('cards');
   Map<String, dynamic>? data;
-  String currentAccount = 'example';
 
   @override
   Widget build(BuildContext context) {
@@ -32,97 +26,89 @@ class _ListState extends State<List> {
         child: Scrollbar(
           child: SingleChildScrollView(
             child: Center(
-              child: Container(
-                child: Column(children: [
-                  OutlinedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => RoomListPage(),
-                      ));
-                    },
-                    child: const Text('Chat'),
-                  ),
-                  StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('cards')
-                        .doc(currentAccount)
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      // 取得が完了していないときに表示するWidget
-                      // if (snapshot.connectionState != ConnectionState.done) {
-                      //   // インジケーターを回しておきます
-                      //   return const CircularProgressIndicator();
-                      // }
-
-                      // エラー時に表示するWidget
+              child: Column(children: <Widget>[
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => RoomListPage(),
+                    ));
+                  },
+                  child: const Text('Chat'),
+                ),
+                FutureBuilder<DocumentSnapshot>(
+                    future: users.doc(uid).get(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<DocumentSnapshot> snapshot) {
                       if (snapshot.hasError) {
-                        print(snapshot.error);
-                        return Text('error');
+                        return const Text("Something went wrong");
                       }
 
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Text("Loading");
+                      if (snapshot.hasData && !snapshot.data!.exists) {
+                        return const Text("Document does not exist");
                       }
 
-                      // データが取得できなかったときに表示するWidget
-                      if (!snapshot.hasData) {
-                        return Text('no data');
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        Map<String, dynamic> user =
+                            snapshot.data!.data() as Map<String, dynamic>;
+
+                        return StreamBuilder<DocumentSnapshot<Object?>>(
+                          stream: cards.doc(user['my_cards'][0]).snapshots(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (snapshot.hasError) {
+                              return const Text('Something went wrong');
+                            }
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Text("Loading");
+                            }
+                            dynamic data = snapshot.data;
+                            final exchangedCards = data?['exchanged_cards'];
+
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: exchangedCards.length,
+                              itemBuilder: (context, index) {
+                                return StreamBuilder<DocumentSnapshot<Object?>>(
+                                  stream: cards
+                                      .doc(exchangedCards[index])
+                                      .snapshots(),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<DocumentSnapshot>
+                                          snapshot) {
+                                    if (snapshot.hasError) {
+                                      return const Text('Something went wrong');
+                                    }
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return const Text("Loading");
+                                    }
+                                    dynamic card = snapshot.data;
+                                    if (!snapshot.hasData) {
+                                      return Text('no data');
+                                    }
+                                    return Column(
+                                      children: [
+                                        Text('username: ${card?['name']}'),
+                                        card?['is_user'] == true
+                                            ? MyCard(cardId: exchangedCards[index])
+                                            : card?['thumbnail'] != null
+                                                ? Image.network(
+                                                    card?['thumbnail'])
+                                                : const Text('Loading...'),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            );
+                          },
+                        );
                       }
-
-                      dynamic hoge = snapshot.data;
-                      final exchangedCards = hoge?['exchanged_cards'];
-                      // 取得したデータを表示するWidget
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: exchangedCards.length,
-                        itemBuilder: (context, index) {
-                          return StreamBuilder(
-                            stream: FirebaseFirestore.instance
-                                .collection('cards')
-                                .doc(exchangedCards[index])
-                                .snapshots(),
-                            builder: (context, snapshot) {
-                              // 取得が完了していないときに表示するWidget
-                              // if (snapshot.connectionState != ConnectionState.done) {
-                              //   // インジケーターを回しておきます
-                              //   return const CircularProgressIndicator();
-                              // }
-                              dynamic data = snapshot.data;
-
-                              // エラー時に表示するWidget
-                              if (snapshot.hasError) {
-                                print(snapshot.error);
-                                return Text('error');
-                              }
-
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Text("Loading");
-                              }
-
-                              // データが取得できなかったときに表示するWidget
-                              if (!snapshot.hasData) {
-                                return Text('no data');
-                              }
-
-                              // 取得したデータを表示するWidget
-                              return Column(
-                                children: [
-                                  Text('username: ${data?['name']}'),
-                                  data?['thumbnail'] != null
-                                      ? Image.network(data?['thumbnail'])
-                                      : const Text('Loading...'),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ]),
-              ),
+                      return const Text("Loading");
+                    }),
+              ]),
             ),
           ),
         ),
@@ -135,7 +121,6 @@ class _ListState extends State<List> {
             ),
           ));
         },
-        // onPressed: getImage,
         child: const Icon(
           Icons.add_a_photo_rounded,
         ),
