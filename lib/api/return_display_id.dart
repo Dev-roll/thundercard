8abb07fd7e114771687b4ebb5633922c6492a4d5
notchcoder <kokiuaxdx@gmail.com>
2022-09-30 05:00:53 +0900

    feat: IDを返す関数の実装

--- lib/api/return_display_id.dart
+++ lib/api/return_display_id.dart
@@ -1,3 +1,15 @@
+import '../constants.dart';
+
 String returnDisplayId(String url) {
-  return url;
+  var ret = url;
+  var linkUrlMap = {...linkTypeToBaseUrl};
+  linkUrlMap.remove('url');
+  linkUrlMap.forEach(
+    (key, value) {
+      if (url.startsWith(value)) {
+        ret = url.substring(value.length);
+      }
+    },
+  );
+  return ret;
 }
