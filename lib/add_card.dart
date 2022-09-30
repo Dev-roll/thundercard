37f4fc82af869e2ea4b400d6827cea354fafad6e
cardseditor <toshiki.u314@gmail.com>
2022-09-30 15:40:32 +0900

    fix: fix bug editing profile

--- lib/add_card.dart
+++ lib/add_card.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
+import 'package:intl/intl.dart';
 
 import 'api/firebase_auth.dart';
 import 'widgets/my_card.dart';
@@ -17,28 +18,61 @@ class AddCard extends StatefulWidget {
   State<AddCard> createState() => _AddCardState();
 }
 
-void handleExchange(String myCardId, otherCardId) async {
+void handleExchange(String myCardId, anotherCardId) async {
   final DocumentReference myCard =
       FirebaseFirestore.instance.collection('cards').doc(myCardId);
-  final DocumentReference otherCard =
-      FirebaseFirestore.instance.collection('cards').doc(otherCardId);
-  final String otherUid = await otherCard.get().then((DocumentSnapshot res) {
+  final DocumentReference anotherCard =
+      FirebaseFirestore.instance.collection('cards').doc(anotherCardId);
+  final String anotherUid =
+      await anotherCard.get().then((DocumentSnapshot res) {
     final data = res.data() as Map<String, dynamic>;
     return data['uid'];
   });
   final Room room = await FirebaseChatCore.instance
-      .createRoom(User.fromJson({'id': otherUid}));
+      .createRoom(User.fromJson({'id': anotherUid}));
 
   myCard.update({
-    'exchanged_cards': FieldValue.arrayUnion([otherCardId]),
-    'rooms.$otherCardId': room.toJson()
+    'exchanged_cards': FieldValue.arrayUnion([anotherCardId]),
+    'rooms.$anotherCardId': room.toJson()
   }).then((value) => print("DocumentSnapshot successfully updated"),
       onError: (e) => print("Error updating document $e"));
-  otherCard.update({
+  anotherCard.update({
     'exchanged_cards': FieldValue.arrayUnion([myCardId]),
     'rooms.$myCardId': room.toJson()
   }).then((value) => print("DocumentSnapshot successfully updated"),
       onError: (e) => print("Error updating document $e"));
+
+  final addMyNotificationData = {
+    'title': '名刺リスト追加のお知らせ',
+    'content': '@$myCardIdさんの名刺リストに@$anotherCardIdさんが追加されました！',
+    'created_at': DateTime.now(),
+    'read': false,
+    'tags': ['interaction'],
+    'id':
+        'list-add-$myCardId-${DateFormat('yyyy-MM-dd-Hm').format(DateTime.now())}',
+  };
+
+  FirebaseFirestore.instance
+      .collection('cards')
+      .doc(myCardId)
+      .collection('notifications')
+      .add(addMyNotificationData);
+      
+  final addAnotherNotificationData = {
+    'title': '名刺リスト追加のお知らせ',
+    'content': '@$anotherCardIdさんの名刺リストに@$myCardIdさんが追加されました！',
+    'created_at': DateTime.now(),
+    'read': false,
+    'tags': ['interaction'],
+    'id':
+        'list-add-$anotherCardId-${DateFormat('yyyy-MM-dd-Hm').format(DateTime.now())}',
+  };
+
+  FirebaseFirestore.instance
+      .collection('cards')
+      .doc(anotherCardId)
+      .collection('notifications')
+      .add(addAnotherNotificationData);
 }
 
 class _AddCardState extends State<AddCard> {
