b6d3e738e8179f2991b799955729ff3d18d46cc8
keigomichi <hundredacrewood1165g7@gmail.com>
2022-10-01 23:51:31 +0900

    feat: add search widget to the list

--- lib/api/firebase_firestore.dart
+++ lib/api/firebase_firestore.dart
@@ -17,10 +17,19 @@ Future<String> getDisplayName(String cardId) async {
       FirebaseFirestore.instance.collection('cards').doc(cardId);
   final String displayName = await card.get().then((DocumentSnapshot res) {
     final data = res.data() as Map<String, dynamic>;
-    return data['account.profiles.name'];
+    return data['account']['profiles']['name'];
   }).catchError((error) {
     print("Failed to add user: $error");
     return 'Cannot get name';
   });
   return displayName;
+}
+
+Future<DocumentSnapshot> getCard(String cardId) async {
+  DocumentReference card =
+      FirebaseFirestore.instance.collection('cards').doc(cardId);
+  final DocumentSnapshot res = await card.get().catchError((error) {
+    print("Failed to add user: $error");
+  });
+  return res;
 }
