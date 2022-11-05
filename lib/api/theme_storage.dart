4544a616521def11d0b95fc528ae29f88649457a
keigomichi <hundredacrewood1165g7@gmail.com>
2022-11-05 17:36:20 +0900

    feat: テーマをローカルに保存 Fixes #398

---
+++ lib/api/theme_storage.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class ThemeStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/appThemeIdx.txt');
+  }
+
+  Future<int> readTheme() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      final contents = await file.readAsString();
+      print('read -> $contents');
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeTheme(int appThemeIdx) async {
+    final file = await _localFile;
+
+    print('write -> $appThemeIdx');
+    // Write the file
+    return file.writeAsString('$appThemeIdx');
+  }
+}
