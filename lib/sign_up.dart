927389c3ae1a579d4047c33d74bb900cd357fc7c
cardseditor <toshiki.u314@gmail.com>
2022-11-19 19:29:25 +0900

    chore: update

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:thundercard/auth_gate.dart';
 import 'package:thundercard/widgets/privacy_policy.dart';
@@ -439,27 +439,27 @@ class _SignUpState extends State<SignUp> {
                     const SizedBox(
                       height: 8,
                     ),
-                    // if (!(kIsWeb || Platform.isIOS || Platform.isMacOS))
-                    // ElevatedButton(
-                    //   style: ElevatedButton.styleFrom(
-                    //     elevation: 0,
-                    //     backgroundColor: Colors.transparent,
-                    //   ),
-                    //   onPressed: () => _onSignInGoogle(),
-                    //   child: Row(
-                    //     mainAxisSize: MainAxisSize.min,
-                    //     mainAxisAlignment: MainAxisAlignment.center,
-                    //     children: const [
-                    //       Icon(
-                    //         FontAwesomeIcons.google,
-                    //         size: 20,
-                    //       ),
-                    //       SizedBox(width: 8),
-                    //       Text('Googleでサインアップ'),
-                    //     ],
-                    //   ),
-                    // ),
-                    if (kIsWeb || !(Platform.isIOS || Platform.isMacOS))
+                    if (!kIsWeb && Platform.isAndroid)
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: Colors.transparent,
+                        ),
+                        onPressed: () => _onSignInGoogle(),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              FontAwesomeIcons.google,
+                              size: 20,
+                            ),
+                            SizedBox(width: 8),
+                            Text('Googleでサインアップ'),
+                          ],
+                        ),
+                      ),
+                    if (kIsWeb)
                       const GoogleSignInButton(
                           clientId:
                               '277870400251-aaolhktu6ilde08bn6cuhpi7q8adgr48.apps.googleusercontent.com'),
