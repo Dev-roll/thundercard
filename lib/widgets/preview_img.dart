83078bdc08aabe3c992bdfa5794de4166065fdc0
notchcoder <kokiuaxdx@gmail.com>
2022-11-27 20:27:26 +0900

    feat: 画像の操作を可能に

---
+++ lib/widgets/preview_img.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class PreviewImg extends StatelessWidget {
+  const PreviewImg({
+    super.key,
+    required this.image,
+  });
+  final Widget image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: InteractiveViewer(
+                    minScale: 0.1,
+                    maxScale: 4,
+                    child: Hero(
+                      tag: 'card_image',
+                      child: image,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(
+                Icons.close_rounded,
+                size: 32,
+                color: white,
+              ),
+              padding: const EdgeInsets.all(12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
