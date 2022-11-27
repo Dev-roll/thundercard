b756f041804c87a605a8dc5edff80a849a387f65
notchcoder <kokiuaxdx@gmail.com>
2022-11-27 15:55:29 +0900

    chore: MdPageへの遷移に変更

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -12,6 +12,7 @@ import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:thundercard/auth_gate.dart';
+import 'package:thundercard/md_page.dart';
 import 'package:thundercard/widgets/privacy_policy.dart';
 import 'package:thundercard/widgets/terms_of_use.dart';
 
@@ -520,7 +521,10 @@ class _SignInState extends State<SignIn> {
                                       Navigator.of(context).push(
                                         MaterialPageRoute(
                                           builder: (context) {
-                                            return const PrivacyPolicy();
+                                            return const MdPage(
+                                              title: Text('プライバシーポリシー'),
+                                              content: PrivacyPolicy(),
+                                            );
                                           },
                                         ),
                                       );
@@ -544,7 +548,10 @@ class _SignInState extends State<SignIn> {
                                       Navigator.of(context).push(
                                         MaterialPageRoute(
                                           builder: (context) {
-                                            return const TermsOfUse();
+                                            return const MdPage(
+                                              title: Text('利用規約'),
+                                              content: TermsOfUse(),
+                                            );
                                           },
                                         ),
                                       );
