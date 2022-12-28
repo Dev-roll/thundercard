4caac4d54659df01008cbce7e25e51e02144cdb8
notchcoder <kokiuaxdx@gmail.com>
2022-12-28 21:04:06 +0900

    fix: 競合の解消

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -181,57 +181,11 @@ class MyCard extends ConsumerWidget {
                                             .popUntil((route) => route.isFirst);
                                         ScaffoldMessenger.of(context)
                                             .showSnackBar(
-                                          SnackBar(
-                                            elevation: 20,
-                                            backgroundColor: Theme.of(context)
-                                                .colorScheme
-                                                .surfaceVariant,
-                                            behavior: SnackBarBehavior.floating,
-                                            clipBehavior: Clip.antiAlias,
-                                            dismissDirection:
-                                                DismissDirection.horizontal,
-                                            margin: EdgeInsets.only(
-                                              left: 8,
-                                              right: 8,
-                                              bottom: MediaQuery.of(context)
-                                                      .size
-                                                      .height -
-                                                  180,
-                                            ),
-                                            duration:
-                                                const Duration(seconds: 2),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(28),
-                                            ),
-                                            content: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                const Padding(
-                                                  padding: EdgeInsets.fromLTRB(
-                                                      0, 0, 16, 0),
-                                                  child: Icon(Icons
-                                                      .file_download_done_rounded),
-                                                ),
-                                                Expanded(
-                                                  child: Text(
-                                                    'カードを交換しました',
-                                                    style: TextStyle(
-                                                        color: Theme.of(context)
-                                                            .colorScheme
-                                                            .onBackground,
-                                                        overflow:
-                                                            TextOverflow.fade),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                            // duration: const Duration(seconds: 12),
-                                            action: SnackBarAction(
-                                              label: 'OK',
-                                              onPressed: () {},
-                                            ),
+                                          PositionedSnackBar(
+                                            context,
+                                            'カードを交換しました',
+                                            icon: Icons
+                                                .file_download_done_rounded,
                                           ),
                                         );
                                       },
