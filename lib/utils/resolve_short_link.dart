888008bbd6fccd04457f043f1b79ef61087095a6
cardseditor <toshiki.u314@gmail.com>
2023-05-10 03:40:52 +0900

    refactor: 不要なファイルを削除

--- lib/utils/resolve_short_link.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-Future<String> resolveShortLink(String shortLink) async {
-  final response = await http.head(Uri.parse(shortLink));
-  final redirectLink = response.headers['location'] ?? '';
-  return redirectLink;
-}
